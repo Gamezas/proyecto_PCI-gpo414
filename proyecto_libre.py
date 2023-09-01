3cb784f050fe6dd801d4a889f4d457b87084eb27
Gamezas <142269176+Gamezas@users.noreply.github.com>
2023-08-31 23:40:43 -0600

    proyecto_libre.py
    Actualización para el segundo avance del código

--- proyecto_libre.py
+++ proyecto_libre.py
@@ -1,43 +1,7 @@
-print("Bienvenido al simplificador de hoja de personaje de D&D")
-eleccion1 = int(input("Selecciona con un número que raza te gustaría jugar" +
- "1. Humano, 2. Tiefling: "))
-
-if eleccion1 == 1:
-    strg_sc = 1
-    dex_sc = 1
-    const_sc = 1
-    smart_sc = 1
-    wisd_sc = 1
-    char_sc = 1
-    Speed = "30 ft"
-    raza = "Humano"
-else:
-    strg_sc = 0
-    dex_sc = 0
-    const_sc = 0
-    smart_sc = 1
-    wisd_sc = 0
-    char_sc = 2
-    speed = "30 ft"
-    raza = "Tiefling"
-
-print ("Bien ahora vayamos con tus valores de habilidad")
-strg = int(input("Tira un dado de 20 caras y valor escribelo aquí" +
-"este será tu valor de fuerza: "))
-
-dex = int(input("Tira otro dado de 20 para tu valor de destreza: "))
-const = int(input("Tira otro dado de 20 para tu valor de constitución: "))
-smart = int(input("Tira otro dado de 20 para tu valor de inteligencia: "))
-wisd = int(input("Tira otro dado de 20 para tu valor de sabiduría: "))
-char = int(input("Tira otro dado de 20 para tu valor de carisma: "))
-
-strg_sc = strg_sc + strg
-dex_sc = dex_sc + dex
-const_sc = const_sc + const
-smart_sc = smart_sc + smart
-wisd_sc = wisd_sc + wisd
-char_sc = char_sc + char
-
+"""
+El objetivo de esta función es calcular el modificador de las habilidades del
+jugador de froma rápida
+"""
 def modificador (num1):
     if num1 >= 0 and num1 <= 1:
         mod = 0 - 5
@@ -75,8 +39,94 @@ def modificador (num1):
     else:
         mod = 0 + 6
         return mod
+"""
+Esta función aunque es simple permite calcular el score total del jugador en 
+una habilidad sumando el score fijo de su rza y lo conseguido en el dado
+"""
+"""
+Esta funcion a pesar de ser simple permite indetificar su uso y facilitar el 
+uso de operadores del código
+"""
+def score (atributo,dado):
+    return atributo + dado
 
+print("Bienvenido al simplificador de hoja de personaje de D&D")
+eleccion1 = int(input("Selecciona con un número que raza te gustaría jugar" +
+ " 1. Humano, 2. Tiefling, 3. Halfling: 4. Dwarf: "))
+"""
+Aquí se coloca la elección de razas lo que provoca que algunas estadisticas 
+tengan ciertos atributos a favor desde un inicio de igual forma ayuda a mantener
+un seguimiento de las mismas
+"""
+if eleccion1 == 1:
+    strg_atr = 1
+    dex_atr = 1
+    const_atr = 1
+    smart_atr = 1
+    wisd_atr = 1
+    char_atr = 1
+    Speed = "30 ft"
+    raza = "Humano"
+elif eleccion1 == 2:
+    strg_atr = 0
+    dex_atr = 0
+    const_atr = 0
+    smart_atr = 1
+    wisd_atr = 0
+    char_atr = 2
+    speed = "30 ft"
+    raza = "Tiefling"
 
+elif eleccion1 == 3:
+    strg_atr = 0
+    dex_atr = 2
+    const_atr = 0
+    smart_atr = 0
+    wisd_atr = 0
+    char_atr = 0
+    speed = "25 ft"
+    raza = "Halfling"
+
+else:
+    strg_atr = 0
+    dex_atr = 0
+    const_atr = 2
+    smart_atr = 0
+    wisd_atr = 0
+    char_atr = 0
+    speed = "25 ft"
+    raza = "Dwarf"
+
+"""
+Las estadísticas de D&D son medidas con una tirada de un dado de 20 caras
+aquí el usuario podra introducir los valores obtenido por sus tiradas y estas
+se añadiran más adelante con los atributos iniciales para así identificar 
+el modificador correcto
+"""
+print ("Bien ahora vayamos con tus valores de habilidad")
+strg = int(input("Tira un dado de 20 caras y valor escribelo aquí" +
+"este será tu valor de fuerza: "))
+
+dex = int(input("Tira otro dado de 20 para tu valor de destreza: "))
+const = int(input("Tira otro dado de 20 para tu valor de constitución: "))
+smart = int(input("Tira otro dado de 20 para tu valor de inteligencia: "))
+wisd = int(input("Tira otro dado de 20 para tu valor de sabiduría: "))
+char = int(input("Tira otro dado de 20 para tu valor de carisma: "))
+
+"""
+Utilizando la función score sacamos el score total de las habilidades del 
+usuario
+"""
+strg_sc = score(strg_atr,strg)
+dex_sc = score(dex_atr,dex)
+const_sc = score(const_atr, const)
+smart_sc = score(smart_atr,smart)
+wisd_sc = score(wisd_atr,wisd)
+char_sc = score(char_atr,char)
+"""
+Utilizando la función modificador conseguimos el valor del modificador de las
+habilidades del usuraio
+"""
 strg_mod = modificador(strg_sc)
 dex_mod = modificador(dex_sc)
 const_mod = modificador(const_sc)
@@ -84,8 +134,12 @@ smart_mod = modificador(smart_sc)
 wisd_mod = modificador(wisd_sc)
 char_mod = modificador(char_sc)
 
+"""
+Se le dan al usuario todas las estadisticas calculadas por el programa
+"""
 print ("Tu personaje esta listo!!")
-print ("Tu raza es" + raza)
+print ("Tu raza es ", raza)
+print ("La velocidad de tu personaje es ", speed)
 print ("Tu score de habilidad en fuerza es: ", strg_sc, "y tu modificador es:",
 strg_mod)
 print ("Tu score de habilidad en destreza es: ", dex_sc, "y tu modificador",
